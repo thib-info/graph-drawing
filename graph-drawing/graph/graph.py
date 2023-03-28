4c19cf0eadc86e45851b8de3ea220594ac7540ec
thib_info <thibault.b45@gmail.com>
2023-03-28 16:35:44 +0200

    Implementation of new graph types

--- graph-drawing/graph/graph.py
+++ graph-drawing/graph/graph.py
@@ -8,12 +8,91 @@ def generate_cycle_graph(n=10, seed=None):
     """
     Generates a simple cycle graph with n nodes.
     """
-    if seed:
+    if seed is not None:
         random.seed(seed)
-    G = nx.cycle_graph(n)
-    data = nx.node_link_data(G)
+
+    graph = nx.cycle_graph(n)
+    data = nx.node_link_data(graph)
+
     return data
 
+
+def generate_tree_graph(n=10, seed=None):
+    """
+    Generates a simple tree graph with n nodes.
+    """
+    if seed is not None:
+        random.seed(seed)
+
+    graph = nx.random_tree(n)
+    data = nx.readwrite.json_graph.node_link_data(graph)
+
+    return data
+
+
+def generate_bipartite_graph(num_vertices=10, num_edges=10, seed=None):
+    """
+    Generates a bipartite graph with given number of vertices and edges.
+    """
+    if seed is not None:
+        random.seed(seed)
+
+    num_left = num_vertices // 2
+    left_nodes = range(num_left)
+    right_nodes = range(num_left, num_vertices)
+    edges = [(random.choice(left_nodes), random.choice(right_nodes)) for _ in range(num_edges)]
+
+    graph = nx.Graph()
+    graph.add_nodes_from(left_nodes, bipartite=0)
+    graph.add_nodes_from(right_nodes, bipartite=1)
+    graph.add_edges_from(edges)
+
+    data = nx.readwrite.json_graph.node_link_data(graph)
+
+    return data
+
+
+def generate_outerplanar_graph(num_vertices=10, seed=None):
+    """
+    Generates an outerplanar graph with given number of vertices.
+    """
+    if seed is not None:
+        random.seed(seed)
+
+    if num_vertices <= 2:
+        raise ValueError("Number of vertices must be greater than 2.")
+
+    # Create a cycle graph with n-2 vertices
+    graph = nx.cycle_graph(num_vertices - 2)
+
+    # Add two new vertices connected to all previous vertices
+    graph.add_node(num_vertices - 2)
+    graph.add_node(num_vertices - 1)
+    for i in range(num_vertices - 2):
+        graph.add_edge(i, num_vertices - 2)
+        graph.add_edge(i, num_vertices - 1)
+
+    data = nx.readwrite.json_graph.node_link_data(graph)
+
+    return data
+
+
+def generate_grid_graph(num_rows, num_cols, seed=None):
+    """
+    Generates a grid graph with given number of rows and columns.
+    """
+    if seed is not None:
+        random.seed(seed)
+
+    graph = nx.grid_2d_graph(num_rows, num_cols)
+
+    # Rename nodes to have consecutive integers
+    mapping = {(i, j): i * num_cols + j for i, j in graph.nodes()}
+    graph = nx.relabel_nodes(graph, mapping)
+
+    data = nx.readwrite.json_graph.node_link_data(graph)
+
+    return data
 
 def save_graph(graph_data, filename):
     """
--- graph-drawing/main.py
+++ graph-drawing/main.py
@@ -9,8 +9,10 @@ def parse_args():
     Parses the command-line arguments for the program.
     """
     parser = argparse.ArgumentParser(description='Generate and visualize graphs.')
-    parser.add_argument('--generate', action='store_true', help='generate a new graph')
-    parser.add_argument('--filename', type=str, default='cycle_graph.json', help='filename for the graph data')
+    parser.add_argument('-g', '--generate', action='store_true', help='generate a new graph')
+    parser.add_argument('-o', '--filename', type=str, default='cycle_graph.json', help='filename for the graph data')
+    parser.add_argument('-gt', '--graph-type', type=str, default="cycle", choices=["all", "cycle", "tree", "bipartite", "outerplanar", "grid"], help="Type of graph to generate")
+    parser.add_argument('-n', '--num-nodes', nargs='+', type=int, help='Number of nodes in the graph')
     return parser.parse_args()
 
 
@@ -18,9 +20,25 @@ def main():
     args = parse_args()
 
     if args.generate:
-        # generate a new cycle graph and save it to a file
-        graph_data = graph.generate_cycle_graph()
-        graph.save_graph(graph_data, args.filename)
+        if args.graph_type == 'cycle' or args.graph_type == 'all':
+            # generate a new cycle graph and save it to a file
+            graph_data = graph.generate_cycle_graph()
+            graph.save_graph(graph_data, 'cycle_graph.json')
+        elif args.graph_type == 'tree' or args.graph_type == 'all':
+            graph_data = graph.generate_tree_graph(args.n)
+            graph.save_graph(graph_data, 'tree_graph.json')
+        elif args.graph_type == 'bipartite' or args.graph_type == 'all':
+            if len(args.num_nodes) > 1:
+                graph_data = graph.generate_bipartite_graph(args.num_nodes[0], args.num_nodes[1])
+            else:
+                graph_data = graph.generate_bipartite_graph()
+            graph.save_graph(graph_data, 'bipartite_graph.json')
+        elif args.graph_type == 'outerplanar' or args.graph_type == 'all':
+            graph_data = graph.generate_outerplanar_graph(args.num_nodes[0])
+            graph.save_graph(graph_data, 'outerplanar_graph.json')
+        elif args.graph_type == 'grid' or args.graph_type == 'all':
+            graph_data = graph.generate_grid_graph(args.num_nodes[0], args.num_nodes[1])
+            graph.save_graph(graph_data, 'grid_graph.json')
 
     # load the graph data from the file
     G = graph.load_graph(args.filename)
@@ -34,5 +52,4 @@ def main():
 
 
 if __name__ == '__main__':
-    print("It worked")
     main()
